21a7665fa6a3f60afad39c1d9759ef5f2cc331da
ixianhao <42532653+ixianhao@users.noreply.github.com>
2024-06-04 17:06:55 +0800

    Update archive.php
    修复在分类和标签页面，文章列表显示异常的问题

--- archive.php
+++ archive.php
@@ -21,37 +21,36 @@
             <?php while($this->next()): ?>
 
             <!-- Post -->
-            <article class="news-item box ">
+        <article class="news-item box">
                 <div class="news-item__image-wrap overlay overlay--45">
-                    <div class="news-item__sort"><span style="color:#fff;">
-                            <?php $this->category('.'); ?>
-                        </span></div>
-                    <div class="news-item__date">
-                        <span>
-                           	<?php $this->date('Y.m.d'); ?>
-                        </span>
-                    </div>
-                    <div class="news-item__con">
-                        <p itemprop="name headline">
-                            <?php $this->title(15, '...') ?>
-                        </p>
-                        <span>
-                            <?php $this->excerpt(20, '...');?>
-                        </span>
-                    </div>
-                    <a class="news-item__link" itemprop="url" href="<?php $this->permalink() ?>"></a>
-                    <?php if($this->options->slimg && 'guanbi'==$this->options->slimg): ?>
-                    <?php else: ?>
-                    <?php if($this->options->slimg && 'showoff'==$this->options->slimg): ?>
-                    <?php else: ?>
-                    <img class="news-item-image cover ls-is-cached lazyloaded" src="<?php showThumbnail($this); ?>"
-                        alt="">
-                    <?php endif; ?>
-                    <?php endif; ?>
-
+                        <a class="news-item__link" itemprop="url" href="<?php $this->permalink() ?>"></a>
+                        <?php if($this->options->slimg && 'guanbi'==$this->options->slimg): ?>
+                        <?php else: ?>
+                        <?php if($this->options->slimg && 'showoff'==$this->options->slimg): ?>
+                        <?php else: ?>
+                        <img class="news-item-image cover ls-is-cached lazyloaded" src="<?php showThumbnail($this); ?>" alt="">
+                        <?php endif; ?>
+                        <?php endif; ?>
+                        <div class="news-item__sort">
+                                <span style="color:#fff;">
+                                        <?php $this->category('.'); ?>
+                                </span>
+                        </div>
+                        <div class="news-item__date">
+                                <span>
+                                        <?php $this->date('M jS'); ?>
+                                </span>
+                        </div>
                 </div>
+                <div class="news-item__caption">
+                        <h2 class="title title--h4">
+                            <?php $this->sticky(); $this->title(20, '') ?>
+                        </h2>
 
-
+                        <p class="news-excerpt" style="font-weight: bold;">
+                            <?php $this->excerpt(48, '');?>
+                        </p>
+                </div>
 
             </article>
 
